299480245d07d5161d46d74d8519b647541d93c8
Amit Einav <amiteinav@google.com>
2019-11-01 16:46:41 +0200

    Update bld_dep.sh

--- bld_dep.sh
+++ bld_dep.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
+number=$RANDOM
 
 echo "build"
-docker build . -t gcr.io/app-immersion-2019-a4242/cropper
+docker build . -t gcr.io/app-immersion-2019-a4242/cropper:${number}
 
 echo "push"
-docker push gcr.io/app-immersion-2019-a4242/cropper
+docker push gcr.io/app-immersion-2019-a4242/cropper:${number}
 
 echo "deploy to cloud run"
-gcloud beta run deploy cropper --image gcr.io/app-immersion-2019-a4242/cropper:latest --platform managed --region us-central1 --project app-immersion-2019-a4242
+gcloud beta run deploy cropper --image gcr.io/app-immersion-2019-a4242/cropper:${number} --platform managed --region us-central1 --project app-immersion-2019-a4242
 
-curl "https://cropper-rjxrs4o6dq-uc.a.run.app?url=https://storage.googleapis.com/cloud-run-bucket/amit-profile-pic.jpg&xmin=10&ymin=10&&ymax=100"
+gsutil rm gs://app-imm-bucket-out/amit-profile-pic-2.jpg
 
-sleep 7
+curl `gcloud beta run  services list --platform managed --project  app-immersion-2019-a4242 |grep cropper | awk '{print $4}'`
 
-gcloud logging read --project app-immersion-2019-a4242  "resource.type=cloud_run_revision AND resource.labels.service_name=cropper" --limit 4
+#curl "https://cropper-rjxrs4o6dq-uc.a.run.app?url=https://storage.googleapis.com/cloud-run-bucket/amit-profile-pic.jpg&xmin=10&ymin=10&&ymax=100"
+
+sleep 1
+
+gsutil ls gs://app-imm-bucket-out 
+
+#gcloud logging read --project app-immersion-2019-a4242  "resource.type=cloud_run_revision AND resource.labels.service_name=cropper" --limit 3
