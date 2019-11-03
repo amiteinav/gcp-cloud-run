b18f98ff3a6544c98a971fc3ce233125920dece7
Amit Einav <amiteinav@google.com>
2019-11-03 10:40:01 +0200

    Update build_and_deploy.sh

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
 
 number=$RANDOM
+project=`gcloud config get-value project`
+bucket=${bucket}
 
-echo "build"
-docker build . -t gcr.io/app-immersion-2019-a4242/cropper:${number}
+echo "build . -t gcr.io/${project}/cropper:${number}"
+docker build . -t gcr.io/${project}/cropper:${number}
 
-echo "push"
-docker push gcr.io/app-immersion-2019-a4242/cropper:${number}
+echo "docker push gcr.io/${project}/cropper:${number}"
+docker push gcr.io/${project}/cropper:${number}
 
-echo "deploy to cloud run"
-gcloud beta run deploy cropper --image gcr.io/app-immersion-2019-a4242/cropper:${number} --platform managed --region us-central1 --project app-immersion-2019-a4242
+echo "gcloud beta run deploy cropper --image gcr.io/${project}/cropper:${number} --platform managed --region us-central1 --project ${project}"
+gcloud beta run deploy cropper --image gcr.io/${project}/cropper:${number} --platform managed --region us-central1 --project ${project}
 
-gsutil rm gs://app-imm-bucket-out/amit-profile-pic-2.jpg
+echo "gsutil rm gs://${bucket}/amit-profile-pic-2.jpg"
+gsutil rm gs://${bucket}/amit-profile-pic-2.jpg
 
-gsutil ls gs://app-imm-bucket-out
+echo "gsutil ls gs://${bucket}"
+gsutil ls gs://${bucket}
 
-curl `gcloud beta run  services list --platform managed --project  app-immersion-2019-a4242 |grep cropper | awk '{print $4}'`
+curl `gcloud beta run  services list --platform managed --project ${project} | grep cropper | awk '{print $4}'`
 
 #curl "https://cropper-rjxrs4o6dq-uc.a.run.app?url=https://storage.googleapis.com/cloud-run-bucket/amit-profile-pic.jpg&xmin=10&ymin=10&&ymax=100"
 
 sleep 1
 
-gsutil ls gs://app-imm-bucket-out 
+gsutil ls gs://${bucket} 
 
-#gcloud logging read --project app-immersion-2019-a4242  "resource.type=cloud_run_revision AND resource.labels.service_name=cropper" --limit 3
+#gcloud logging read --project ${project}  "resource.type=cloud_run_revision AND resource.labels.service_name=cropper" --limit 3
