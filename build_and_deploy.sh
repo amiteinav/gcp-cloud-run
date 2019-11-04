7986b12c51babb560dc9fcaf5de2c46b02b3200c
Amit Einav <amiteinav@google.com>
2019-11-04 19:26:06 +0200

    Update build_and_deploy.sh

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -15,8 +15,8 @@ docker push gcr.io/${project}/cropper:${number}
 echo "gcloud beta run deploy cropper --image gcr.io/${project}/cropper:${number} --platform managed --allow-unauthenticated --region us-central1 --project ${project}"
 gcloud beta run deploy cropper --image gcr.io/${project}/cropper:${number} --platform managed --allow-unauthenticated --region us-central1 --project ${project}
 
-echo "gsutil rm gs://${bucket}/amit-profile-pic-crop.jpg"
-gsutil rm gs://${bucket}/amit-profile-pic-crop.jpg
+echo "gsutil rm gs://${bucket}/pic-crop.jpg"
+gsutil rm gs://${bucket}/pic-crop.jpg
 
 echo "gsutil ls gs://${bucket}"
 gsutil ls gs://${bucket}
