17c565d8f4e9681fd8dcf6334c7d772537d84941
Amit Einav <amiteinav@google.com>
2019-11-04 21:20:22 +0200

    Update build_and_deploy.sh

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -15,24 +15,12 @@ docker push gcr.io/${project}/cropper:${number}
 echo "gcloud beta run deploy cropper --image gcr.io/${project}/cropper:${number} --platform managed --allow-unauthenticated --region us-central1 --project ${project}"
 gcloud beta run deploy cropper --image gcr.io/${project}/cropper:${number} --platform managed --allow-unauthenticated --region us-central1 --project ${project}
 
-echo "gsutil rm gs://${bucket}/pic-crop.jpg"
-gsutil rm gs://${bucket}/pic-crop.jpg
-
-echo "gsutil ls gs://${bucket}"
-gsutil ls gs://${bucket}
-
 URL=`gcloud beta run  services list --platform managed --project ${project} | grep cropper | awk '{print $4}'`
 
-URL=${URL}"?url=https://storage.googleapis.com/app-imm-bucket-in/amit-profile-pic.jpg&top=300&right=352&left=760&bottom=1100"
+#URL=${URL}"?url=https://storage.googleapis.com/app-imm-bucket-in/amit-profile-pic.jpg&top=352&right=760&left=300&bottom=1101"
+URL=${URL}"?url=https://upload.wikimedia.org/wikipedia/commons/d/da/Guido-portrait-2014.jpg&top=300&right=1100&left=300&bottom=1100"
 
 echo "curl $URL"
 curl $URL
 
-
-#curl "https://cropper-rjxrs4o6dq-uc.a.run.app?url=https://storage.googleapis.com/cloud-run-bucket/amit-profile-pic.jpg&xmin=10&ymin=10&&ymax=100"
-
-sleep 1
-
 gsutil ls -l gs://${bucket} 
-
-#gcloud logging read --project ${project}  "resource.type=cloud_run_revision AND resource.labels.service_name=cropper" --limit 3
