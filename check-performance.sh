9052cec4358a0ee7aa5cb47b2933c11e99968bfa
Amit Einav <amiteinav@google.com>
2019-11-08 11:15:52 +0200

    Create check-performance.sh

---
+++ check-performance.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "CTRL+C to stop"
+
+URL=`gcloud beta run  services list --platform managed --project ${project} | grep cropper | awk '{print $4}'`
+URL=${URL}"?url=https://upload.wikimedia.org/wikipedia/commons/d/da/Guido-portrait-2014.jpg&top=300&right=1100&left=300&bottom=1100"
+
+
+while [ 0 ] ; do
+    curl $URL > /dev/null &
+done
